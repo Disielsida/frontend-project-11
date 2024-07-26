ef8440ad71357eb0dc059b9038829ee8b6ae7129
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2024-07-26 23:49:09 +0300

    try12321

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import data from './data.js';
 import './style.scss';
 import 'bootstrap';
 
-const dataDouble = data.push(2);
+const root = document.getElementById('app');
 
-export default dataDouble;
+data.forEach((element) => {
+  const item = document.createElement('div');
+  item.textContent = element;
+  item.classList.add('item');
+
+  root.appendChild(item);
+});
