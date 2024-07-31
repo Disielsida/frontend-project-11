0233293e5a93b9bc3b8dcde8ccbbe7d4f095abbf
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2024-07-31 23:36:44 +0300

    i18next adding

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  ru: {
+    translation: {
+      errors: {
+        notValid: 'Ссылка должна быть валидным URL',
+        hasAlready: 'RSS уже существует',
+        emptyField: 'Ничего нет',
+      },
+    },
+  },
+};
--- src/view.js
+++ src/view.js
@@ -1,12 +1,13 @@
 import onChange from 'on-change';
 
-export default (state, elements) => {
+export default (state, elements, i18nextInstance) => {
   const { input, form, feedback } = elements;
 
   return onChange(state, (path, value) => {
     if (state.form.error !== '') {
       input.classList.add('is-invalid');
-      feedback.textContent = value;
+      feedback.textContent = i18nextInstance.t(value);
+      console.log(i18nextInstance.t(value), value, i18nextInstance.t('errors.notValid'));
     } else {
       input.classList.remove('is-invalid');
       feedback.textContent = '';
